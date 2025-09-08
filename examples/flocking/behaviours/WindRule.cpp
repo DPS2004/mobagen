3b98e465dadb39d996efa03bc49d4873ee569a6a
DPS2004 <dpsophinos@gmail.com>
2025-09-07 21:36:05 -0400

    add wind

--- examples/flocking/behaviours/WindRule.cpp
+++ examples/flocking/behaviours/WindRule.cpp
@@ -7,7 +7,9 @@
 Vector2f WindRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   // todo: add a wind force here
   // hint: use the windAngle variable
-  return Vector2f::zero();
+  Vector2f force = Vector2f::up();
+  force = force.Rotate(windAngle * (180.0 / M_PI));
+  return force;
 }
 
 bool WindRule::drawImguiRuleExtra() {
