ae113469c88e968b4c647e1a68fd7f97f8c96a8d
DPS2004 <dpsophinos@gmail.com>
2025-10-06 13:24:59 -0400

    catcher tries to end game faster once trapped

--- examples/catchthecat/Catcher.cpp
+++ examples/catchthecat/Catcher.cpp
@@ -1,5 +1,7 @@
 #include "Catcher.h"
 #include "World.h"
+#include <algorithm>
+#include <random>
 
 Point2D Catcher::Move(World* world) {
   auto side = world->getWorldSideSize() / 2;
@@ -7,10 +9,25 @@ Point2D Catcher::Move(World* world) {
   if (!path.empty()) {
     return path.front();
   }
+  std::cout <<"CATCHER has trapped cat, now trying to end game.\n";
+  auto cat = world->getCat();
+  std::vector<Point2D> neighbors;
+  neighbors.push_back(World::NE(cat));
+  neighbors.push_back(World::NW(cat));
+  neighbors.push_back(World::E(cat));
+  neighbors.push_back(World::W(cat));
+  neighbors.push_back(World::SE(cat));
+  neighbors.push_back(World::SW(cat));
+  std::ranges::shuffle(neighbors, std::random_device{});
+  for (auto n : neighbors) {
+    if (!world->getContent(n)) {
+      return n;
+    }
+  }
+
   std::cout <<"CATCHER RANDOM\n";
   for (;;) {
     Point2D p = {Random::Range(-side, side), Random::Range(-side, side)};
-    auto cat = world->getCat();
     if (cat.x != p.x && cat.y != p.y && !world->getContent(p)) return p;
   }
 }
